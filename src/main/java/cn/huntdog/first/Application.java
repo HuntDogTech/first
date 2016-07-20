fd56ee94d9e2fd821a621e54a51fba8c63fd0d5b
JonDai <jondai@aliyun.com>
2016-07-20 11:36:56 +0800

    添加WebSocket聊天室原型

--- src/main/java/cn/huntdog/first/Application.java
+++ src/main/java/cn/huntdog/first/Application.java
@@ -2,16 +2,27 @@ package cn.huntdog.first;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
 
 /**
  * Created by JonDai {jondai@aliyun.com} on 2016/3/10.
  */
 
-//发现注解@Scheduled的任务并后台执行
 //@EnableScheduling
 @SpringBootApplication
-public class Application {
+public class Application implements EmbeddedServletContainerCustomizer {
     public static void main(String[] args){
         SpringApplication.run(Application.class, args);
+    }
+
+
+    /**
+     * 通过实现EmbeddedServletContainerCustomizer接口来自定义启动项
+     * @param configurableEmbeddedServletContainer
+     */
+    @Override
+    public void customize(ConfigurableEmbeddedServletContainer configurableEmbeddedServletContainer) {
+        configurableEmbeddedServletContainer.setPort(8081);
     }
 }
